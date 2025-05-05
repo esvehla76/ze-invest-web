bf4f32c3327329fcb3ba160f7d2fbef2453b26b8
esvehla76 <159880519+esvehla76@users.noreply.github.com>
2025-05-05 10:33:09 +0200

    Oprava výstupního adresáře pro Cloudflare Pages

--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'build', // Zmìna výstupního adresáøe z 'dist' na 'build'
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
